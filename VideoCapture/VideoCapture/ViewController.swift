27e252ed92c10fa0720c1250c104299eefba1a9e
liwei <liwei19880505@126.com>
2022-07-27 00:19:01 +0800

    add video capture project

---
+++ VideoCapture/VideoCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by liwei on 2022/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
